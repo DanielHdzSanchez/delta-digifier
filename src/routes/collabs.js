08c5d3f333099fea188694d79a6bdb52fcf81f12
DanielHdzSanchez <daniel.hdz1427@gmail.com>
2021-04-08 15:54:03 -0500

    First try on appointments

--- src/routes/collabs.js
+++ src/routes/collabs.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const timestampToDate = require('../lib/dates')
 
 const db = require('../database')
 
-
+router.get('/', async (req, res) => {
+    try {
+        const appointments = await db.query('SELECT * FROM appointment')
+        res.render('collabs/list', {appointments})
+    } catch (error) {
+        console.log('Could not find appointments', error)
+        res.send('There was an error')
+    }
+})
 
 module.exports = router
